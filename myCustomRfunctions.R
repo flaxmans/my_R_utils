35b3b9a0d27e999492bed4b8af494d8fde85ef83
flaxmans <sam.flaxman@gmail.com>
2017-03-05 21:51:44 -0700

    added new function for getting unused filenames

--- myCustomRfunctions.R
+++ myCustomRfunctions.R
@@ -6,16 +6,21 @@ mySafeWriteCSV <- function ( data = NULL, namebase = NULL, writeRowNames = FALSE
   } else {
     # strip off file extension if present:
     namebase <- strsplit(namebase, "\\.")[[1]][1] 
-    # create first name to try with extension:
-    filename <- paste(namebase, ".csv", sep = "") 
-    counter <- 1 # variable for making new names in a logical sequence
-    # use a while loop to find an unused name:
-    while ( file.exists(filename) ) {
-      filename <- paste(namebase, counter, ".csv", sep = "")
-      counter <- counter + 1
-    }
+    filename <- getUnusedFilename(namebase, ".csv")
     cat(paste("Writing file to '", filename, "'\n", sep = ""))
     # finally, actually write the data to a file:
     write.csv(data, filename, row.names = writeRowNames)
   }
+}
+
+getUnusedFilename <- function(namebase, fileext = ".csv") {
+  # create first name to try with extension:
+  filename <- paste(namebase, fileext, sep = "") 
+  counter <- 1 # variable for making new names in a logical sequence
+  # use a while loop to find an unused name:
+  while ( file.exists(filename) ) {
+    filename <- paste(namebase, counter, fileext, sep = "")
+    counter <- counter + 1
+  }
+  return(filename)
 }
